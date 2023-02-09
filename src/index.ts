6cb61a07e4e357ece94978df4a29c0e7ddfbfe20
Mogy1945 <hatano14150@gmail.com>
2023-02-09 20:00:56 +0900

    コードの修正を行いました。

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ const path = require("path");
 //必要画像数
 const imgNumber = 10;
 
-for (let i = 1; i <= imgNumber; i++) {
-  textToImage.generate(`No${i}`, {
+for (let i = 0; i < imgNumber; i++) {
+  textToImage.generate(`${i}`, {
     debug: true,
-    customHeight: 400, //高さ
-    maxWidth: 400, //幅
-    fontSize: 30, //文字の大きさ
+    customHeight: 100, //高さ
+    maxWidth: 100, //幅
+    fontSize: 20, //文字の大きさ
     textAlign: "center", //行の並び
     verticalAlign: "center", //列の並び
-    debugFilename: path.join("src", "img", `No${i}.png`), //ファイル保存設定
+    debugFilename: path.join("src", "img", `${i}.png`), //ファイル保存設定
   });
 }
